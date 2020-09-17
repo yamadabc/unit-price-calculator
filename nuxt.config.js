5f17cb1a8fbda8d1329d0339e83ea50c97eeff7e
yamadabc <57384471+yamadabc@users.noreply.github.com>
2020-09-17 17:48:51 +0900

    title追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
   */
   head: {
     titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    title: '坪単価計算表',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
