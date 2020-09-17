e66457c3855905341fe96a20bda1d046a1725869
yamadabc <57384471+yamadabc@users.noreply.github.com>
2020-09-17 11:14:40 +0900

    create project

---
+++ components/Calculator.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <p><input type="text" v-model="price" placeholder="価格" />万円</p>
+        <p><input type="text" v-model="area" placeholder="延床面積" />㎡</p>
+        <button @click='calculate(price,area)'>go!</button>
+        <p>坪単価：{{ data.unitPrice }}万円</p>
+    </section>
+</template>
+
+<script>
+    import Vue from 'vue'
+    export default Vue.extend({
+        data: function () {
+            return {
+                data: {
+                    price: '',
+                    area: '',
+                    unitPrice: '',
+                },
+            }
+        },
+        methods: {
+            calculate(price, area) {
+                this.data.unitPrice = Math.round(price / (area / 3.30578));
+            },
+        }
+    })
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+  <h1>坪単価計算</h1>
+		<div class="container">
+			<h2>売買</h2>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+		</div>
+		<div class='container'>
+			<h2>賃貸</h2>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+			<div class="row">
+				<calculator></calculator>
+			</div>
+		</div>
+</div>
+</template>
+
+<script>
+import Vue from 'vue'
+	import calculator from '~/components/Calculator.vue'
+
+	export default Vue.extend({
+		components: {
+			calculator
+		},
+		data: function () {
+			return {
+				data: {
+					price: '',
+					area: '',
+					unitPrice: '',
+				},
+			}
+		},
+	})
+</script>
